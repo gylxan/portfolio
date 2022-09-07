82f8c3ddd1b4b335714de8772a72bc8f580766e6
Guido Lange <g.lange@thalia.de>
2022-09-07 09:07:31 +0200

    Remove ThemeProvider from next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Footer from '../components/Footer/Footer'
-import Header from '../components/Header/Header'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Footer from '../components/Footer/Footer';
+import Header from '../components/Header/Header';
 
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-import { ThemeProvider } from 'next-themes'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
-config.autoAddCss = false
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider enableSystem>
+    <>
       <Header />
       <main>
         <Component {...pageProps} />
       </main>
       <Footer />
-    </ThemeProvider>
-  )
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
