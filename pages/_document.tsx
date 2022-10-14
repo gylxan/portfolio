877e74f29c82503c2c4c89fc5eb13122d31936c9
Guido Lange <g.lange@thalia.de>
2022-10-14 21:54:23 +0200

    Add more meta tags and improve image loading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content={process.env.NEXT_PUBLIC_NAME} />
+        <meta name="keywords" content={process.env.NEXT_PUBLIC_KEYWORDS} />
+        <meta
+          name="description"
+          content={process.env.NEXT_PUBLIC_DESCRIPTION}
+        />
+        <meta name="og:title" content={process.env.NEXT_PUBLIC_URL} />
+        <meta
+          name="og:description"
+          content={process.env.NEXT_PUBLIC_DESCRIPTION}
+        />
+        <meta name="og:url" content={process.env.NEXT_PUBLIC_URL} />
+        <meta name="og:type" content="website" />
+        <meta name="og:image" content={process.env.NEXT_PUBLIC_LOGO_URL} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,52 @@
-import type { NextPage } from 'next';
 import Page from '../components/Page/Page';
 
 import AnimatedTitle from '../components/AnimatedTitle/AnimatedTitle';
-import { ConfigKey, Format, getConfig } from '../utils/config';
 import Button from '../components/Button/Button';
 import Image from 'next/image';
 import Link from '../components/Link/Link';
 import { parseHtml } from '../utils/htmlParse';
+import { parseJSON } from '../utils/json';
 
-interface HomeProps {
-  name: string;
-  profileImageUrl: string;
-  introduction: string[];
-}
-
-const Home: NextPage<HomeProps> = ({ name, profileImageUrl, introduction }) => {
+const Home = () => {
+  const name = process.env.NEXT_PUBLIC_NAME ?? '';
+  const blurImageUrl =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcum1nPQAG8QKl/SZJzwAAAABJRU5ErkJggg==';
   return (
-    <Page title="Portfolio" description="Portfolio of Guido Lange">
+    <Page title="Home">
       <div className="container flex flex-col items-center justify-center gap-4">
         <div className="flex w-full items-center justify-center">
           <AnimatedTitle title={name} subTitle="Frontend Developer" />
         </div>
-        <Link href="/about" coloredHover={false} underlined={false}>
+        <Link
+          href="/about"
+          coloredHover={false}
+          underlined={false}
+          className="relative overflow-hidden rounded-full h-[300px]"
+          aria-label="Profile image with a link to the about page"
+        >
           <Image
-            src={profileImageUrl}
+            src={process.env.NEXT_PUBLIC_PROFILE_IMAGE_URL ?? blurImageUrl}
             alt="profile-image"
-            className="rounded-full opacity-80 hover:opacity-100"
+            className="opacity-80 hover:opacity-100"
             width={300}
             height={300}
-            priority={true}
+            placeholder="blur"
+            blurDataURL={blurImageUrl}
+            priority
           />
         </Link>
 
         <p className="text-secondary">Hi, my name is</p>
         <h2 className="text-3xl font-medium md:text-4xl lg:text-5xl">{name}</h2>
-        {introduction.map((intro) => (
-          <p key={intro}>{parseHtml(intro)}</p>
-        ))}
+        {parseJSON<string[]>(process.env.NEXT_PUBLIC_START_PARAGRAPHS, []).map(
+          (intro) => (
+            <p key={intro}>{parseHtml(intro)}</p>
+          ),
+        )}
         <Button href="/about">Check me out!</Button>
       </div>
     </Page>
   );
-};
-
-export const getStaticProps = () => {
-  return {
-    props: {
-      name: getConfig(ConfigKey.NAME, ''),
-      profileImageUrl: getConfig(ConfigKey.PROFILE_IMAGE_URL, ''),
-      introduction: getConfig(ConfigKey.START_PARAGRAPHS, [], Format.JSON),
-    },
-  };
 };
 
 export default Home;
