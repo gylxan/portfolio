89414a9ac9029b22e03990aada27f39289452c37
Guido Lange <g.lange@thalia.de>
2022-10-18 10:54:42 +0200

    Install next-pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['drive.google.com', 'lh3.googleusercontent.com'],
   },
-};
+});
 
 module.exports = nextConfig;
