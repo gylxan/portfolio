cd515320ce8c728ed47e2cf4019d35aa02a4d56c
Guido Lange <g.lange@thalia.de>
2022-10-27 09:37:29 +0200

    Use margin auto for left and right to prevent overlapsing

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -23,8 +23,8 @@ const Experience = () => {
   return (
     <Page title="Experience">
       <AnimatedTitle title="Experience" />
-      <div className="container mt-8 flex justify-center">
-        <Tabs aria-label="Job Tabs" className="max-w-3xl">
+      <div className="container mt-8">
+        <Tabs aria-label="Job Tabs" className="max-w-3xl mx-auto">
           {experiences.map(({ company, url, positions }) => (
             <Tab key={company} title={company}>
               <h2 className="mb-3 text-[24px]">
