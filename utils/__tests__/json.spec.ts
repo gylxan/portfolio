2d30b39b6351f1885687ae1902c0b212d79077b8
Guido Lange <g.lange@thalia.de>
2022-11-01 07:36:17 +0100

    Add tests for utils

---
+++ utils/__tests__/json.spec.ts
@@ -0,0 +1,18 @@
+import { parseJSON } from '../json';
+
+describe('JSON utils', () => {
+  describe('parseJSON', () => {
+    const json = '{"attribute": "value"}';
+    it('should return the parsed JSON', () => {
+      expect(parseJSON(json, {})).toStrictEqual({ attribute: 'value' });
+    });
+
+    it('should return the default value, when string is empty', () => {
+      expect(parseJSON('', {})).toStrictEqual({});
+    });
+
+    it('should return the default value, when string is undefined', () => {
+      expect(parseJSON(undefined, {  })).toStrictEqual({});
+    });
+  });
+});
