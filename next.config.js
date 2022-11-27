4a46e587637f3ab269d3fb4dfd9ede486ea09956
Guido Lange <guidolange90@gmail.com>
2022-11-27 12:14:49 +0100

    Remove obsolete skipWaiting option for next-pwa

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 /** @type {import('next').NextConfig} */
@@ -11,8 +10,8 @@ const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['res.cloudinary.com'],
-  },
+    domains: ['res.cloudinary.com']
+  }
 });
 
 module.exports = nextConfig;
