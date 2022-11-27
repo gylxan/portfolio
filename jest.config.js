bdde7a6a7d5d4cc1dedb8e153126f815253f3a1b
Guido Lange <guidolange90@gmail.com>
2022-11-27 11:50:36 +0100

    Adjust jest to new setup from Bext 13

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+module.exports = createJestConfig({
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   moduleNameMapper: {
     '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
   testEnvironment: 'jest-environment-jsdom',
-}
+});
