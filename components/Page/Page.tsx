7e74efd514ba2d9d7ca3b34415d69a580401a598
Guido Lange <guidolange90@gmail.com>
2022-12-01 17:31:17 +0100

    Remove empty space for classname in Page

--- components/Page/Page.tsx
+++ components/Page/Page.tsx
@@ -19,8 +19,8 @@ const Page = ({ fullHeight, title, className, ...props }: PageProps) => (
       className={clsx(
         'container mx-auto px-4 md:px-8',
         fullHeight
-          ? ' flex h-full flex-col justify-center'
-          : ' mt-4 mb-6 md:mt-12',
+          ? 'flex h-full flex-col justify-center'
+          : 'mt-4 mb-6 md:mt-12',
         className,
       )}
     />
