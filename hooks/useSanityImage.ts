0f5fe7b8dea3e20ffbffdd2069f673a99f2e9250
Guido Lange <guidolange90@gmail.com>
2022-12-01 15:55:09 +0100

    Add new hook to get sanity image props

---
+++ hooks/useSanityImage.ts
@@ -0,0 +1,13 @@
+import { useNextSanityImage } from 'next-sanity-image';
+import client from '../utils/sanity';
+import type { SanityImageObject } from '@sanity/image-url/lib/types/types';
+
+const useSanityImage = (image: SanityImageObject | null) => {
+  const imageProps = useNextSanityImage(client, image);
+  if (!image || !image.asset) {
+    return null;
+  }
+  return imageProps;
+};
+
+export default useSanityImage;
