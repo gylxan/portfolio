8ce32ee530bc11922712fbf3d67260ec46f0ea05
Guido Lange <guidolange90@gmail.com>
2022-12-02 21:12:21 +0100

    Exclude studio/index.html from pwa build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/studio\/index\.html/]
+
 });
 
 /** @type {import('next').NextConfig} */
