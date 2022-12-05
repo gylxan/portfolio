656f1c73f9baa671bb9d963e85f3af8954146ed0
Guido Lange <guidolange90@gmail.com>
2022-12-05 15:00:42 +0100

    Add studio folder to public excludes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   disable: process.env.NODE_ENV === 'development',
-  buildExcludes: [/\/studio(.*)$/]
+  buildExcludes: [/\/studio(.*)$/],
+  publicExcludes: ['!studio/**/*']
 
 });
 
