65b5b068e2f90e73415929094451ab84625413f4
Guido Lange <guidolange90@gmail.com>
2022-12-05 14:54:21 +0100

    Exclude everything with studio from next-pwa build

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   disable: process.env.NODE_ENV === 'development',
-  buildExcludes: ["/studio/index.html"]
+  buildExcludes: [/\/studio(.*)$/]
 
 });
 
