16ad0f813b817106ccfc9840331928204c94424d
Guido Lange <guidolange90@gmail.com>
2022-12-06 17:09:56 +0100

    preconnect and dns-prefetch sanity.io cdn

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://cdn.sanity.io/" />
+        <link rel="dns-prefetch" href="https://cdn.sanity.io/" />
+      </Head>
       <body>
         <Main />
         <NextScript />
