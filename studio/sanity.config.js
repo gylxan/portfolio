cf01ae06270cfa65937737695d5696577da317f6
Guido Lange <guidolange90@gmail.com>
2022-12-13 14:03:03 +0100

    Add vision again to sanity

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { schemaTypes } from './schemas/schema';
 import { codeInput } from '@sanity/code-input';
+import { visionTool } from '@sanity/vision';
 
 export default defineConfig({
   name: 'portfolio',
@@ -10,13 +11,21 @@ export default defineConfig({
   dataset: import.meta.env.SANITY_STUDIO_DATASET,
   basePath: '/studio',
 
-  plugins: [deskTool(), codeInput()],
+  plugins: [
+    deskTool({}),
+    codeInput(),
+    visionTool({
+      // Note: These are both optional
+      defaultApiVersion: 'v2021-10-21',
+      defaultDataset: import.meta.env.SANITY_STUDIO_DATASET,
+    }),
+  ],
   schema: {
     types: schemaTypes,
   },
   form: {
     images: {
-      directUploads: true
-    }
-  }
+      directUploads: true,
+    },
+  },
 });
