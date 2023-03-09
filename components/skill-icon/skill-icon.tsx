7a7a0a34826da67eebef2f186694676415d65860
Guido Lange <guidolange90@gmail.com>
2023-03-09 17:37:41 +0100

    Use sizes and priority

--- components/skill-icon/skill-icon.tsx
+++ components/skill-icon/skill-icon.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
-import { blurImageUrl } from 'constants/image';
-import { Skill } from 'types/skill';
+import type { Skill } from 'types/skill';
 
 export type SkillIconProps = Skill;
 
 const SkillIcon = ({ url, name }: SkillIconProps) => {
   const image = (
     <Image
-      height="32"
-      width="32"
+      sizes="32px"
       className="w-12 text-primary transition-all duration-300 hover:scale-125"
       src={`https://cdn.simpleicons.org/${name.toLowerCase()}/ABAFB9FF`}
       alt={`Icon of skill ${name}`}
       data-testid="skill-icon"
+      priority
     />
   );
 
