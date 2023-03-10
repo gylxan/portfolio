0f1de23c8552f7f9f46324a0d35bf3163216cfa6
Guido Lange <guidolange90@gmail.com>
2023-03-10 10:35:50 +0100

    Allow column in column and row in row

--- studio/schemas/objects/row.ts
+++ studio/schemas/objects/row.ts
@@ -11,7 +11,7 @@ export default {
       type: 'array',
       title: 'Content',
       description: 'Content',
-      of: [...pageContent.filter(({ type }) => type !== 'row')],
+      of: [...pageContent],
     },
     {
       name: 'space',
