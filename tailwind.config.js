ffd89ae618f161073873ece5f0c086cd27f6b6cd
Guido Lange <guidolange90@gmail.com>
2023-03-20 15:05:43 +0100

    Remove unused pop-in animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,6 @@ module.exports = {
         glitch: 'glitch 1s linear infinite',
         'glitch-bottom': 'glitch-bottom 1.5s linear infinite',
         'glitch-top': 'glitch-top 1s linear infinite',
-        'pop-in': 'pop-in 1.5s linear forwards',
       },
       keyframes: {
         'fade-in': {
@@ -63,38 +62,6 @@ module.exports = {
           },
           '62%': {
             transform: 'translate(-22px, 5px) skew(21deg)',
-          },
-        },
-        'pop-in': {
-          'from,20%,40%,60%,80%,to': {
-            animationTimingFunction: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-          },
-
-          '0%': {
-            opacity: 0,
-            transform: 'scale3d(0.3, 0.3, 0.3)',
-          },
-
-          '20%': {
-            transform: 'scale3d(1.5, 1.5, 1.5)',
-          },
-
-          '40%': {
-            transform: 'scale3d(0.9, 0.9, 0.9)',
-          },
-
-          '60%': {
-            opacity: 1,
-            transform: 'scale3d(1.03, 1.03, 1.03)',
-          },
-
-          '80%': {
-            transform: 'scale3d(0.97, 0.97, 0.9)',
-          },
-
-          to: {
-            opacity: 1,
-            transform: 'scale3d(1, 1, 1)',
           },
         },
       },
