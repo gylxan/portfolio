4968c75ff4c7a90f8453fdee69eea2b8da04821f
Guido Lange <guidolange90@gmail.com>
2023-03-23 12:09:54 +0100

    Add tests for post list

--- components/post-list/post-list.spec.tsx
+++ components/post-list/post-list.spec.tsx
@@ -1,10 +1,32 @@
 import { Post } from 'types/post';
 import { act, render, screen } from '@testing-library/react';
-import PostList from 'components/post-list/post-list';
-import * as hooks from 'hooks/useSanityImage';
+import { PostList } from 'components';
+import * as useSanityImageHook from 'hooks/useSanityImage';
+import * as useEndlessScrollingHook from 'hooks/useEndlessScrolling';
 import { ImageLoader } from 'next/image';
+import * as AppContext from 'contexts/app-context';
 
 jest.mock('hooks/useSanityImage');
+jest.mock('hooks/useEndlessScrolling');
+
+jest.mock('contexts/app-context', () => {
+  const originalAppContext = jest.requireActual('contexts/app-context');
+  return {
+    ...originalAppContext,
+    useAppContext: jest.fn(),
+  };
+});
+
+jest.mock('react-intersection-observer', () => {
+  const actual = jest.requireActual('react-intersection-observer');
+  return {
+    ...actual,
+    useInView: jest.fn().mockReturnValue({
+      ref: jest.fn(),
+      inView: false,
+    }),
+  };
+});
 
 const mockPosts: Post[] = [
   {
@@ -65,11 +87,41 @@ const mockPosts: Post[] = [
   },
 ];
 describe('<PostList/>', () => {
-  jest.spyOn(hooks, 'default').mockReturnValue({
+  const useAppContextSpy = jest.spyOn(AppContext, 'useAppContext');
+  const setData = jest.fn();
+
+  jest.spyOn(useSanityImageHook, 'default').mockReturnValue({
     loader: undefined as unknown as ImageLoader,
     src: 'http://url/image.png',
     width: 123,
     height: 123,
+  });
+
+  let onLoadedMock: (results: Post[], lastId: string | null) => void;
+
+  const useEndlessScrollingHookSpy = jest.spyOn(
+    useEndlessScrollingHook,
+    'default',
+  );
+
+  beforeEach(() => {
+    useAppContextSpy.mockReturnValue({
+      data: {
+        post: { entries: mockPosts, lastId: '' },
+        project: { entries: [], lastId: '' },
+      },
+      setData,
+    });
+
+    useEndlessScrollingHookSpy.mockImplementation(({ onLoaded }) => {
+      onLoadedMock = onLoaded;
+      return {
+        hasMore: false,
+        loading: false,
+        fetchNextPage: jest.fn(),
+        error: null,
+      };
+    });
   });
 
   afterEach(() => {
@@ -82,20 +134,34 @@ describe('<PostList/>', () => {
 
   it('should render', async () => {
     await act(() => {
-      render(<PostList posts={mockPosts} />);
+      render(<PostList />);
     });
 
-    expect(
-      screen.getByText(`post.amount (count: ${mockPosts.length})`),
-    ).toBeInTheDocument();
-    expect(screen.getAllByRole('link')).toHaveLength(2);
+    expect(screen.getAllByRole('link')).toHaveLength(mockPosts.length);
   });
 
   it('should render a message, when there are no posts', async () => {
+    useAppContextSpy.mockReturnValue({
+      data: {
+        post: { entries: [], lastId: '' },
+        project: { entries: [], lastId: '' },
+      },
+      setData,
+    });
     await act(() => {
-      render(<PostList posts={[]} />);
+      render(<PostList />);
     });
 
     expect(screen.getByText('post.no_posts_available')).toBeInTheDocument();
+  });
+
+  it('calls setData of app context, when endless scrolling loaded next page', async () => {
+    await act(() => {
+      render(<PostList />);
+    });
+
+    onLoadedMock?.(mockPosts, null);
+
+    expect(setData).toHaveBeenCalledTimes(1);
   });
 });
