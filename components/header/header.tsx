c379f16c1b1bff5f4c7654829a02b281a0ed8629
Guido Lange <guidolange90@gmail.com>
2023-03-24 10:02:52 +0100

    Remove unused routes constant

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,9 @@
-import { Routes } from 'constants/routes';
 import Image from 'next/image';
 import { LanguageDropdown, Link, Menu } from 'components';
 import useSanityImage from 'hooks/useSanityImage';
 import type { SanityAltImage } from 'types/image';
 import type { MenuLink } from 'types/siteConfig';
-import { SanityFile } from 'types/file';
+import type { SanityFile } from 'types/file';
 
 export interface HeaderProps {
   menuLinks: MenuLink[];
@@ -17,7 +16,7 @@ const Header = ({ logo, menuLinks, resume }: HeaderProps) => {
   return (
     <header className="flex h-24 w-full grow items-center justify-between gap-4 px-4 text-center md:px-8">
       <Link
-        href={Routes.Home}
+        href="/"
         data-testid="logo"
         className="relative h-10 w-10 p-2 text-white hover:text-white "
         underlined={false}
