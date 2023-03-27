0ce863c7e98be27242754ac4b142ba03603c9117
Guido Lange <guidolange90@gmail.com>
2023-03-27 12:18:58 +0200

    Respect language in URL for server sitemap

--- pages/server-sitemap.xml/index.tsx
+++ pages/server-sitemap.xml/index.tsx
@@ -3,17 +3,20 @@ import { GetServerSideProps } from 'next';
 import client from 'utils/sanity';
 import { Post as IPost } from 'types/post';
 import { pathPostQuery } from 'constants/groq';
+import { getLanguageUrlPrefix } from 'utils/url';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const posts = await client.fetch<IPost[] | null>(pathPostQuery);
 
   return getServerSideSitemap(
     ctx,
-    posts?.map(({ slug }) => ({
-      loc: `${process.env.NEXT_PUBLIC_URL}/posts/${slug.current}`,
+    posts?.map(({ slug, language }) => ({
+      loc: `${process.env.NEXT_PUBLIC_URL}${getLanguageUrlPrefix(
+        language,
+      )}/post/${slug.current}`,
       lastmod: new Date().toISOString(),
       changefreq: 'daily',
-      priority: 0.7
+      priority: 0.7,
     })) || [],
   );
 };
