cf9c071a7132e3327729f65633b4671c1a05be7f
Guido Lange <guidolange90@gmail.com>
2023-12-02 13:43:00 +0100

    test: adjust tests for new labels and links

--- components/skill-icon/skill-icon.spec.tsx
+++ components/skill-icon/skill-icon.spec.tsx
@@ -1,7 +1,7 @@
 import SkillIcon, {SkillIconProps} from 'components/skill-icon/skill-icon';
 import {render, screen} from '@testing-library/react';
 
-describe('<SlugIcon />', () => {
+describe('<SkillIcon />', () => {
   const props: SkillIconProps = {
     name: 'javascript',
   };
@@ -11,7 +11,7 @@ describe('<SlugIcon />', () => {
 
     expect(screen.getByRole('img')).toBeInTheDocument();
     expect(screen.getByRole('img')?.getAttribute('src')).toMatch(
-      encodeURIComponent(`https://cdn.simpleicons.org/${props.name.toLowerCase()}/ABAFB9FF`),
+      encodeURIComponent(`https://cdn.simpleicons.org/${props.name.toLowerCase()}/50555F/ABAFB9`),
     );
     expect(screen.queryByRole('link')).not.toBeInTheDocument();
   });
